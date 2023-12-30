fba57274ace332504c9d810782d3e07e25dabdc1
owennguyen3162 <nguyentuan3162@gmail.com>
2023-12-31 01:32:22 +0700

    Popular Dishes Horizontal List

---
+++ FoodOrdering/CustomViews/CollectionViewCell/PopularDishesCollectionViewCell/PopularDishesCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PopularDishesCollectionViewCell.swift
+//  FoodOrdering
+//
+//  Created by nguyenanhtuan on 30/12/2023.
+//
+
+import UIKit
+
+class PopularDishesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ FoodOrdering/Modals/Dish.swift
@@ -0,0 +1,8 @@
+//
+//  Dish.swift
+//  FoodOrdering
+//
+//  Created by nguyenanhtuan on 30/12/2023.
+//
+
+import Foundation
